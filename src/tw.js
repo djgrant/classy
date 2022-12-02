452eac2eba983a65bd8707dbe54140fbddd9b48a
Daniel Grant <1670902+djgrant@users.noreply.github.com>
2022-12-02 11:46:21 +0000

    Support transient props

--- src/tw.js
+++ src/tw.js
@@ -5,8 +5,13 @@ import { tags } from "./tags";
 export const tw = (tag) => (...args) =>
   forwardRef(({ as, ...props }, ref) => {
     const twClasses = typeof args[0] === "function" ? args[0](props) : args;
+    const nonTransientProps = {};
+    for (const [k, v] of Object.entries(props)) {
+      if (k.startsWith("$")) continue;
+      nonTransientProps[k] = v;
+    }
     return createElement(as || tag, {
-      ...props,
+      ...nonTransientProps,
       ref,
       className: classnames(twClasses, props.className),
     });
