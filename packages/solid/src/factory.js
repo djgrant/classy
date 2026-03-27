0cae190a32c773d3572cf19d288169f5d383bfcd
djgrant <1670902+djgrant@users.noreply.github.com>
2026-03-27 20:53:45 +0000

    Update factory.js

--- packages/solid/src/factory.js
+++ packages/solid/src/factory.js
@@ -5,7 +5,7 @@ import {
   getDisplayName,
   hoistStatics,
   resolveClassNames,
-} from "../../core/src/index.js";
+} from "@djgrant/classy-core";
 import { createComponent, Dynamic } from "solid-js/web";
 
 /**
