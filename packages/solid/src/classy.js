9170ec522c71241502e8727e9e21016cfb110dc6
djgrant <1670902+djgrant@users.noreply.github.com>
2026-03-28 19:18:14 +0000

    replace depc

--- packages/solid/src/classy.js
+++ packages/solid/src/classy.js
@@ -1,6 +1,11 @@
-import { createDynamic } from "solid-js/web";
+import { createComponent, Dynamic } from "solid-js/web";
 import { createClassySolid } from "./factory.js";
 
-export const classy = createClassySolid((tag, props) =>
-  createDynamic(() => tag, props),
-);
+export const classy = createClassySolid((tag, props) => {
+  Object.defineProperty(props, "component", {
+    enumerable: true,
+    get: () => tag,
+  });
+
+  return createComponent(Dynamic, props);
+});
